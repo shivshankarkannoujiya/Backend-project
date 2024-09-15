07b41c4c8a5301c92d58159f80956df06fd6f59c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-09-15 19:05:28 +0530

    add util codes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
-dotenv.config()
+dotenv.config();
 
 connectDB()
+  .then(() => {
+      app.listen(process.env.PORT || 3000, () => {
+        console.log(`🍏 App is listening at: ${process.env.PORT}`);
+      });
+  })
+  .catch((error) => {
+    console.log(`MongoDB Connection Failed!!: ${error}`);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+// TODO: Using Promises
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => {() => {}} or
+// const asyncHandler = (fn) => async() => {} or
+
+
+// TODO: Using Try catch
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: true,
+//             message: error.message
+//         })
+//     }
+// }
+
