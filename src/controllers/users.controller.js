50e911d47fc11745824cca17cbed5e24b629c1c5
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-09-25 23:04:51 +0530

    add register controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,11 +1,82 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 
-const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "ok"
-    })
-} )
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
 
 
-export {registerUser}
+  // Validation
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+
+
+  // Check if user already Exist
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }]
+  })
+
+  if (existedUser) {
+      throw new ApiError(409, "User with email or username already exist")
+  }
+
+
+
+  // Check for avatar
+  const avatarLocalPath =  req.files?.avatar[0]?.path;
+  const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is required")
+  }
+
+
+  // Upload on Cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+
+  // check for avatar after uploading
+  if (!avatar) {
+      throw new ApiError(400, "Avatar file is required")
+  }
+
+
+  // Create user Object : Create entry in db
+  const user = await User.create({
+      fullName,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email,
+      password,
+      username: username.toLowerCase()
+  })
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering User")
+  }
+
+  
+  return res.status(201).json(
+      new ApiResponse(
+        200,
+        createdUser,
+        "User Registered Successfully"
+      )
+  )
+
+})
+
+export { registerUser }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 connectDB()
   .then(() => {
       app.listen(process.env.PORT || 3000, () => {
-        console.log(`⚙️  App is listening at: ${process.env.PORT}`);
+        console.log(`⚙️ App is listening at: ${process.env.PORT}`);
       });
   })
   .catch((error) => {
