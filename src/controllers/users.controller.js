66d9dedecfef9d21591366d1fa47044004e61811
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-09-28 15:54:28 +0530

    Fix bugs

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+// import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 
@@ -29,9 +30,38 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
+  // Check for avatar
+  // console.log('Files: ', req.files);
+
+
+
+  // const avatarLocalPath =  req.files?.avatar[0]?.path;
+  // const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+
+
+  // console.log('Avatar Local Path:', avatarLocalPath); // Add this line for debugging
+  // if (!avatarLocalPath) {
+  //     throw new ApiError(400, "Avatar file is required!!!")
+  // }
+
+
+  // Upload on Cloudinary
+  // const avatar = await uploadOnCloudinary(avatarLocalPath)
+  // const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  // console.log('Uploaded Avatar:', avatar);
+  
+
+  // check for avatar after uploading
+  // if (!avatar) {
+  //     throw new ApiError(400, "Avatar file is required")
+  // }
+
+
   // Create user Object : Create entry in db
   const user = await User.create({
       fullName,
+      // avatar: avatar.url,
+      // coverImage: coverImage?.url || "",
       email,
       password,
       username: username.toLowerCase()
