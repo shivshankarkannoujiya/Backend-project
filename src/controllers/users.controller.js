246a01225ae05f257e8989eeb24ae11062267266
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-09-30 17:11:41 +0530

    Add getUserChannelProfile endpoint

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -282,6 +282,94 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
 })
 
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+  const {username} = req.params
+
+  if (!username?.trim()) {
+      throw new ApiError(400, "username is missing")
+  }
+
+  const channel = await User.aggregate([
+
+      {
+        $match: {
+          username: username?.toLowerCase()
+        }
+      },
+
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+        }
+      },
+
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        }
+      },
+
+      {
+        $addFields: {
+
+          subscribersCount: {
+            $size: "$subscribers"
+          },
+
+          channelSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+
+          isSubscribed: {
+            $cond: {
+              if: {$in: [req.user?._id, "$subscribers.subscriber" ]},
+              then: true,
+              else: false
+            }
+          }
+
+        }
+      },
+
+      {
+        $project: {
+
+          fullName: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          email: 1,
+
+        }
+      }
+
+  ])
+
+  if (!channel?.length) {
+      throw new ApiError(404, "Channel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      channel[0],
+      "User Channel Fetched Successfully"
+    )
+  )
+
+})
+
+
 export {
 
     registerUser,
@@ -290,6 +378,7 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    getUserChannelProfile
 }
 
